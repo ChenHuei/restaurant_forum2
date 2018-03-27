80d659a661b215bd6a9f5574dbe1420a8a13d13f
Huei <aaa830714@gmail.com>
2018-03-27 13:35:07 +0800

    Add restaurant new and create actions and views for admin.

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -7,4 +7,29 @@ class Admin::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created."
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create."
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    # 加上 require(:photo) 可以幫我們確認使用者上傳的參數名為 photo，以防邏輯出錯
+    # 透過 permit(:name, :opening_hours, :tel, :address, :description) 
+    # 過濾餐廳名字、營業時間等資料，主要是為了防止有人傳入不相干的惡意資訊。這是一個 Rails 內建的安全性功能，稱為 strong parameters。
+  end
+
 end
