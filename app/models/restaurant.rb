05eeaa74385232dc83b9bce3c03826aaf257ae08
Huei <aaa830714@gmail.com>
2018-03-27 17:17:12 +0800

    Link category and restaurant model.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  belongs_to :category
   mount_uploader :image, PhotoUploader
   # 屬性名稱的型態是 symbol，而 uploader 的名稱型態為 constant
   #（因為 PhotoUploader 也是一個 Class）
