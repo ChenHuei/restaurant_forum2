bb12662b71007f301330b1b444dca4545050a150
Huei <aaa830714@gmail.com>
2018-03-27 12:30:53 +0800

    Set up admin authentication.

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,8 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+  # 我們先確保使用者有登入，才能拿到 User instance
+  # 進而去驗證 instance 身上攜帶的 role 屬性，是否帶有 "admin" 的值
 
   def index
     
