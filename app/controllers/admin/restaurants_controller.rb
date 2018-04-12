fd40a3f47486899a36a438aa3cad5cdd88761f54
Huei <aaa830714@gmail.com>
2018-04-12 17:47:10 +0800

    Add Admin::BaseController and move authebticate_admin here.

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,4 @@
-class Admin::RestaurantsController < ApplicationController
-  before_action :authenticate_admin
+class Admin::RestaurantsController < Admin::BaseController
   # 我們先確保使用者有登入，才能拿到 User instance
   # 進而去驗證 instance 身上攜帶的 role 屬性，是否帶有 "admin" 的值
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
