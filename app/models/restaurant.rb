7ad61f2c0d3d0116f5d0c4ebe4c201f13b1cee46
Huei <aaa830714@gmail.com>
2018-04-13 12:49:01 +0800

    Create comment model and setup relation between restaurant,user and comment.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :category
+  has_many :comments
   mount_uploader :image, PhotoUploader
   # 屬性名稱的型態是 symbol，而 uploader 的名稱型態為 constant
   #（因為 PhotoUploader 也是一個 Class）
