2a6541904beabfc068d56857f0848e0979a1fbcc
Huei <aaa830714@gmail.com>
2018-04-13 16:30:21 +0800

    Add user name to registration form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
+  validates_presence_of :name
+  # 確保 name 欄位為非空集合
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
