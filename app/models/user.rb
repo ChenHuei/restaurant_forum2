dd8cd47150808df293191b300bc131814256263c
Huei <aaa830714@gmail.com>
2018-04-13 15:56:13 +0800

    User can uploader avatar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  mount_uploader :avatar, AvatarUploader
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
