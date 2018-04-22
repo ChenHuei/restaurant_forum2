d5d0774a9788ba37c49b83ea944f3552c160718a
Huei <aaa830714@gmail.com>
2018-04-22 13:11:25 +0800

    User can view commented restaurants on profile page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # 確保 name 欄位為非空集合
   mount_uploader :avatar, AvatarUploader
   has_many :comments
+  has_many :restaurants, through: :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
